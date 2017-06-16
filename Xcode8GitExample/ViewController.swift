3919a731b6bc7df3aaaf76fcfbc5caea0b88c0df
Sunil Narwal <snarwal@gmail.com>
2017-06-16 15:25:00 +0530

    Initial Commit

---
+++ Xcode8GitExample/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Xcode8GitExample
+//
+//  Created by Sunil Narwal on 6/16/17.
+//  Copyright © 2017 Sunil Narwal. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
